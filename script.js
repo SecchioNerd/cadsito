0bcda84941a2d32617eac6f55f39fc1f97661206
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-07-27 19:47:34 +0200

    Add external script for button

--- script.js
+++ script.js
@@ -3,18 +3,3 @@ function associazione() {
     if (confirm(messaggio)) alert('Sei entrato con successo nella nostra associazione! Sono 72€ per l\'entrata.');
     else alert('Ci dispiace che tu non ci voglia vendere la tua anima.');
 }
-
-const themeToggleButton = document.getElementById('themeToggleButton');
-
-// Apply saved theme (if any)
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-    document.body.classList.add('dark');
-    themeToggleButton.textContent = '☀️ Light mode';
-};
-// Toggle and save to click
-themeToggleButton.addEventListener('click', () => {
-    const isDark = document.body.classList.toggle('dark');
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    themeToggleButton.textContent = isDark ? '☀️ Light mode' : '🌙 Dark mode';
-});
