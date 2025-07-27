7bf4ef9137ee02c473b777837630f8b1f346217e
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-07-27 19:00:47 +0200

    Add light/dark mode button

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function associazione() {
     if (confirm(messaggio)) alert('Sei entrato con successo nella nostra associazione! Sono 72€ per l\'entrata.');
     else alert('Ci dispiace che tu non ci voglia vendere la tua anima.');
 }
+
+const themeToggleButton = document.getElementById('themeToggleButton');
+
+themeToggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')) themeToggleButton.textContent = '☀️ Modalità chiara';
+    else themeToggleButton.textContent = '🌙 Modalità scura';
+});
