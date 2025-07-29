5fc22876c81614262a1d01d3e9eedec2ecaad0ab
Toniolo Lorenzo <220308515+SecchioNerd@users.noreply.github.com>
2025-07-29 17:09:57 +0200

    Merge branch 'main' of https://github.com/SecchioNerd/cadsito

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function associazione() {
     if (confirm(messaggio)) alert('Sei entrato con successo nella nostra associazione! Sono 72€ per l\'entrata.');
     else alert('Ci dispiace che tu non ci voglia vendere la tua anima.');
 }
+
+
+// Fatto da SecchioNerd
+
+let i=0;
+
+function menu(){
+
+    if(i%2==0){
+        document.getElementById("menu").innerHTML = '<div style="display: flex;"><a href="#cadman" class="indice">Chi è CADman?</a> <br><a href="#chiSiamo" class="indice">Chi siamo?</a> <br><a href="#associazione" class="indice">Vuoi far parte della nostra associazione?</a> <br><a href="#moltiUtentiNonSanno" class="indice">Molti dei nostri utenti non sanno che...</a> <br><a href="#faq" class="indice" style="border-right:red solid 3px">FAQ</a></div>';
+    }
+    else{
+        document.getElementById("menu").innerHTML = '<div style="display: none;"><a href="#cadman" class="indice">Chi è CADman?</a> <br><a href="#chiSiamo" class="indice">Chi siamo?</a> <br><a href="#associazione" class="indice">Vuoi far parte della nostra associazione?</a> <br><a href="#moltiUtentiNonSanno" class="indice">Molti dei nostri utenti non sanno che...</a> <br><a href="#faq" class="indice" style="border-right:red solid 3px">FAQ</a></div>';
+    }
+
+    i++;
+}
